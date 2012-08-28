0798dcaf6ac19f9cf7170a4fa65733eaf71ebeb4
vsoshnikov <dedok.mad@gmail.com>
2012-08-29 01:54:26 +0400

    added packages to repo

---
+++ README.txt
@@ -0,0 +1,8 @@
+Sructure.
+	t2h\doc
+	t2h\src
+	t2h\toolchains
+	t2g\
+Building.
+	To build t2h under Mac OS X/Lunix platforms 
+
---
+++ src/cmake_extra/osx/config.cmake
@@ -0,0 +1,5 @@
+set(BOOST_ROOT /Users/dedokOne/local/boost/boost_1_47_0/boost)
+set(LIBTORRENT_ROOT /Users/dedokOne/workspace/src-archs/libtorrent-rasterbar-0.16.1)
+set(libtorrent_INCLUDE_DIR ${LIBTORRENT_ROOT}/include)
+set(libtorrent_LIBS ${LIBTORRENT_ROOT}/libtorrent-rasterbar.a)
+
