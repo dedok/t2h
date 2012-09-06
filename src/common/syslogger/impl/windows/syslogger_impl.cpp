2267469e24212d4a9e87f981840ea0e617e6e004
dedok <dedok.mad@gmail.com>
2012-09-07 02:48:50 +0400

    #10 fixed wrong convert from std::string::c_str() to LPCSTR

--- src/common/syslogger/impl/windows/syslogger_impl.cpp
+++ src/common/syslogger/impl/windows/syslogger_impl.cpp
@@ -7,17 +7,19 @@
 		
 } // namespace details
 #define MAX_STRINGS_PER_REPORT 1
-#define REPORT_EVENT_GENERIC(handle, type, message) 								 	\
-	if (handle != INVALID_HANDLE_VALUE)													\
-		ReportEventA(handle, 															\
-					type, 																\
-					0, 																	\
-					0, 																	\
-					NULL,																\
-					MAX_STRINGS_PER_REPORT, 											\
-					0, 																	\
-					(LPCSTR *)message, 													\
-					NULL); 
+#define REPORT_EVENT_GENERIC(handle, type, message)	\
+	if (handle != INVALID_HANDLE_VALUE)	{			\
+		char const * message_ = message.c_str();	\
+		ReportEventA(handle,						\
+					type, 							\
+					0, 								\
+					0, 								\
+					NULL,							\
+					MAX_STRINGS_PER_REPORT, 		\
+					0, 								\
+					(LPCSTR *)&message_, 			\
+					NULL); 							\
+	}											
 
 /**
  * Public syslogger_impl api
@@ -36,19 +38,19 @@ syslogger_impl::~syslogger_impl()
 void syslogger_impl::error(std::string const & message) 
 {
 	REPORT_EVENT_GENERIC(log_handle_, 
-		EVENTLOG_ERROR_TYPE, message.c_str())
+		EVENTLOG_ERROR_TYPE, message)
 }
 
 void syslogger_impl::warning(std::string const & message) 
 {
 	REPORT_EVENT_GENERIC(log_handle_, 
-		EVENTLOG_WARNING_TYPE, message.c_str())
+		EVENTLOG_WARNING_TYPE, message)
 }
 
 void syslogger_impl::note(std::string const & message) 
 {
 	REPORT_EVENT_GENERIC(log_handle_, 
-		EVENTLOG_INFORMATION_TYPE, message.c_str())
+		EVENTLOG_INFORMATION_TYPE, message)
 }
 
 void syslogger_impl::trace(std::string const & message) 
@@ -61,8 +63,8 @@ void syslogger_impl::trace(std::string const & message)
  */
 void syslogger_impl::init_event_source() 
 {
-	std::string const event_soutce_name = settings_.ident + "_" + settings_.facility;
-	log_handle_ = RegisterEventSource(NULL, (LPCSTR)event_soutce_name.c_str());
+	std::string const event_source_name = settings_.ident + "_" + settings_.facility;
+	log_handle_ = RegisterEventSource(NULL, (LPCSTR)event_source_name.c_str());
 	if (GetLastError() == ERROR_ACCESS_DENIED)
 		log_handle_ = INVALID_HANDLE_VALUE;
 }
