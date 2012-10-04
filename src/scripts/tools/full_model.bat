0debf7da0b946112f5761976fdd2eb06280f48a3
dedok <dedok.mad@gmail.com>
2012-10-04 11:09:29 +0400

    Improve error test

--- src/scripts/tools/full_model.bat
+++ src/scripts/tools/full_model.bat
@@ -2,7 +2,9 @@
 
 set pwd=%~dp0
 set error_message=""
-set full_model_path=%pwd%\bin\full_model
+set lib_path=%pwd%lib
+set bin_path=%pwd%bin
+set full_model_path=%bin_path%\full_model
 set full_model_args=--with-config=%pwd%t2h_envt\my_config.json --torrents-dir=%pwd%t2h_envt\torrents
 set full_model_command=%full_model_path% %full_model_args%
 set build_path=shared_Debug_build
@@ -14,7 +16,11 @@ if not exist %pwd%t2h_envt (
 goto build_and_run_t2h
 	
 :build_and_run_t2h
-	call cd %build_path% && nmake full_model && cd %pwd% || echo WARNING: build new binary failed.
+	call cd %build_path% && nmake full_model || echo WARNING: build new binary failed.
+	cd %pwd%
+	if %errorlevel% EQU 0 (
+		copy /Y %lib_path%\t2h_core* %bin_path%
+		)
 	call %full_model_command% || echo ERROR: %full_model_command% failed
 goto EOF
 
