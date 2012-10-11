91b4cad1e7a6c2b2d2998131a55cdb100c52369a
vsoshnikov <dedok.mad@gmail.com>
2012-10-11 15:15:34 +0400

    Fixed broken tests

--- src/tests/core_tests/link_test.cpp
+++ src/tests/core_tests/link_test.cpp
@@ -5,15 +5,15 @@
 int main(char ** argv, int argc) 
 {
 	t2h_init(NULL);
-	t2h_close(NULL);
-	t2h_add_torrent(NULL, NULL);
-	t2h_add_torrent_url(NULL, NULL);
-	t2h_get_torrent_files(NULL, -1);
-	t2h_start_download(NULL, -1, -1);	
-	t2h_paused_download(NULL, -1, -1);
-	t2h_resume_download(NULL, -1, -1);
-	t2h_delete_torrent(NULL, -1);
-	t2h_stop_download(NULL, -1);
+	t2h_close(-1);
+	t2h_add_torrent(-1, NULL);
+	t2h_add_torrent_url(-1, NULL);
+	t2h_get_torrent_files(-1, -1);
+	t2h_start_download(-1, -1, -1);	
+	t2h_paused_download(-1, -1, -1);
+	t2h_resume_download(-1, -1, -1);
+	t2h_delete_torrent(-1, -1);
+	t2h_stop_download(-1, -1);
 	return 0;
 }
 
